1477407b306ef6ae42435df979faf608d194a47f
igor2533 <igor25_33@mail.ru>
2021-07-27 22:03:48 +0300

    edit port app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 let server = require('http').createServer(app);
-const PORT = 8080;
+const PORT = process.env.PORT;
 let io = require('socket.io')(server);
 
 let segments = [];
